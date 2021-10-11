bea801e935a34987cf175b56902ca2e2533e76fb
anggitrisi <anggitrisihombing@gmail.com>
2021-10-11 20:46:49 +0700

    edit 01-asynchronous answer

--- 07-Javascript-Intermediate/08-asynchronous/01-asynchronous.js
+++ 07-Javascript-Intermediate/08-asynchronous/01-asynchronous.js
@@ -28,12 +28,12 @@ second log:  6
 */
 // 5. Apabila terdapat ketidak samaan antara console.log pertama dan kedua, bisakah kalian memberikan penjelasan mengapa hal tersebut bisa terjadi
 /*
-Saat proses asynchronous(dalm hal ini fungsi setTimeOut()) mulai berjalan, proses for loop akan segera berjalan tanpa selesai. Hal ini karena for loop tidak mengunggu proses asynchronous selesai sebelum melanjutkan iterasi selanjutnya dan funsi setTimeOut dipanggil dibeberapa waktu kedepan.dan pada saati itu index loop(i) akan memiliki final value (i=6) untuk diberikan pada fungsi setTimeOut
+Saat proses asynchronous(dalam hal ini fungsi setTimeOut()) mulai berjalan, proses for loop akan segera berjalan sampai selesai. Hal ini karena for loop tidak mengunggu proses asynchronous selesai sebelum melanjutkan iterasi selanjutnya dan fungsi setTimeOut dipanggil dibeberapa waktu kedepan.Dan pada saat itu index loop(i) akan memiliki final value (i=6) untuk diberikan pada fungsi setTimeOut, ini dikarenakan deklarasi i dibuat dengan var yang mana memiliki sifar global scope. Oleh karena itu untuk mencegah ini pendeklarasian i dengan var diganti menjadi let sebab let akan bersifat lokal scope dan i yang diberikan pada setTimeOut akan sama dengan iterasi.
 */
 
 // 6. Perbaiki baris kode dibawah sehingga ia akan menampilkan angka yang sama
 // mengganti var menjadi let, let variabel dari ES6 membuat scope terpisah untuk blok kode yang memungkinkan loop untuk mencetak variabel berurutan.
-for (let i = 1; i<=5; i++) {
+for (let i = 1; i <= 5; i++) {
   console.log("first log: ", i); // 01 - Pertama
   setTimeout(() => console.log("second log: ", i), 100); // 02 - Kedua
 }
