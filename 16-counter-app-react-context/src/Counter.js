af0c9548385f31ac7d5537742a1cff1cb760b74c
anggitrisi <anggitrisihombing@gmail.com>
2021-11-08 21:54:55 +0700

    add 16-react-context

---
+++ 16-counter-app-react-context/src/Counter.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+import { useCount } from "./CountContext";
+
+const Counter = () => {
+  // set dispatch and state count from useCount()
+  const { dispatch, state } = useCount();
+
+  const onInc = () => {
+    dispatch({ type: "increment", state });
+  };
+
+  const onDec = () => {
+    dispatch({ type: "decrement", state });
+  };
+
+  return (
+    <>
+      {/* change 0 with count from context */}
+      Count: {state.count}
+      <div>
+        {/* use dispatch for onClick method */}
+        <button
+          className="count-button"
+          onClick={() => {
+            onInc();
+          }}
+        >
+          +
+        </button>
+        <button
+          className="count-button"
+          onClick={() => {
+            onDec();
+          }}
+        >
+          -
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default Counter;
